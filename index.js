a03622065893eb2a9f0caacf51d3080da2cbdb71
ImAlAmin <alamin.cse97@gmail.com>
2023-03-24 11:06:54 +0600

    added to create a server

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /*
-Title : 
-Description:
+Title : Uptime Monitoring Application
+Description: A RESTFul API to monitor up or down time of user defined links
 Author: Md.Al-Amin
 Date: 24/03/23
 */
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding 
+const app = {};
+
+// configuration
+app.config = {};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer();
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+    // response handle 
+    res.end('Hello World');
+}
