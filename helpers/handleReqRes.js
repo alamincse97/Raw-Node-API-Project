7ee0678a26d1b639a197822309dd9e18862a1537
ImAlAmin <alamin.cse97@gmail.com>
2023-03-25 11:19:14 +0600

    added a part one

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -5,10 +5,13 @@ Author: Md.Al-Amin
 Date: 24/03/23
 */
 
-// dependenceies
+// dependencies
 const url = require('url');
-const {StringDecoder} = require('string_decoder');
-// module scaffolding
+const { StringDecoder } = require('string_decoder');
+const routes = require('../routes');
+const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
+
+// modue scaffolding
 const handler = {};
 
 handler.handleReqRes = (req, res) => {
@@ -16,24 +19,46 @@ handler.handleReqRes = (req, res) => {
     // get the url and parse it
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g)
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     const method = req.method.toLowerCase();
     const queryStringObject = parsedUrl.query;
     const headersObject = req.headers;
 
+    const requestProperties = {
+        parsedUrl,
+        path,
+        trimmedPath,
+        method,
+        queryStringObject,
+        headersObject,
+    };
+
     const decoder = new StringDecoder('utf-8');
     let realData = '';
 
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+
+    chosenHandler(requestProperties, (statusCode, payload) => {
+        statusCode = typeof statusCode === 'number' ? statusCode : 500;
+        payload = typeof payload === 'object' ? payload : {};
+
+        const payloadString = JSON.stringify(payload);
+
+        // return the final response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    });
+
     req.on('data', (buffer) => {
-        realData += decoder.end();
+        realData += decoder.write(buffer);
     });
 
     req.on('end', () => {
         realData += decoder.end();
 
         console.log(realData);
-        // response handle 
-    res.end('Hello Programmer');
+        // response handle
+        res.end('Hello world');
     });
 };
 
--- routes.js
+++ routes.js
@@ -4,3 +4,12 @@ Description: Application Routes
 Author: Md.Al-Amin
 Date: 25/03/23
 */
+
+// dependencies
+const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
+
+const routes = {
+    sample: sampleHandler,
+};
+
+module.exports = routes;
