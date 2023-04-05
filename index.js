f1923e67a2c952d2a74944f218585b9031481e7b
ImAlAmin <alamin.cse97@gmail.com>
2023-04-05 12:30:20 +0600

    added to request data parse

--- index.js
+++ index.js
@@ -6,18 +6,20 @@
 */
 // dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 3000;
+    port: 3000
 };
 
 // create server
 app.createServer = () => {
-    const server = http.createServer(handleReqRes);
+    const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
         console.log(`listening to port ${app.config.port}`);
     });
@@ -25,8 +27,30 @@ app.createServer = () => {
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello World');
+    // request handling
+    // get the url and parse it 
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        console.log(realData);
+        // response handle
+        res.end('Hello World');
+    });
+    
 };
 
 // start the server
