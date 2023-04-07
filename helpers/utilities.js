9136bf2ac6f83e797ac7f423de4502be1fd4f785
ImAlAmin <alamin.cse97@gmail.com>
2023-04-07 16:58:12 +0600

    added to part 4 complete

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -9,9 +9,9 @@
 // dependencies
 
 // module scaffolding
-const utilities = {};
-const crypto = require('crypto'); 
+const crypto = require('crypto');
 
+const utilities = {};
 const environments = require('./environments');
 
 // parse JSON string to Object
@@ -33,6 +33,25 @@ utilities.hash = (str) => {
         console.log(environments, process.env.NODE_ENV);
         const hash = crypto.createHmac('sha256', environments.secretKey).update(str).digest('hex');
         return hash;
+    }
+    return false;
+};
+
+// create random string
+utilities.createRandomString = (strlength) => {
+    let length = strlength;
+    length = typeof strlength === 'number' && strlength > 0 ? strlength : false;
+
+    if (length) {
+        const possiblecharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possiblecharacters.charAt(
+                Math.floor(Math.random() * possiblecharacters.length)
+            );
+            output += randomCharacter;
+        }
+        return output;
     }
     return false;
 };
--- routes.js
+++ routes.js
@@ -6,12 +6,14 @@
 */
 
 // dependencies
-const {sampleHandler} = require('./handlers/routeHandlers/sampleHandlers');
-const  {userHandler} = require('./handlers/routeHandlers/userHandler');
+const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
+const { userHandler } = require('./handlers/routeHandlers/userHandler');
+const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
 
 const routes = {
     sample: sampleHandler,
-    user: userHandler
+    user: userHandler,
+    token: tokenHandler,
 };
 
 module.exports = routes;
